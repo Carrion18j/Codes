f585d36b9c8bcc3af5f90879c0acbd7e3718b88d
Carrion18j <carrion18j@gmail.com>
2023-03-05 11:37:04 +0530

    d

--- .vscode/Projects/Project_no6_LoginPage/script.js
+++
@@ -1,9 +0,0 @@
-const btn = (btn)=>{
-    return btn = document.querySelector('.LoginBut')
-}
-
-btn.onClick = ()=>{
-    document.getElementById('username').innerHTML = '';
-    document.getElementById('gmail').innerHTML = '';
-    document.getElementById('password').innerHTML = '';
-}
