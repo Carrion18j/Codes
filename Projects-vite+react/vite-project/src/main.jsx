bff5190fdfb6e9fa7f664ba90d193c8bba15a381
Carrion <Carrion18j@gmail.com>
2023-03-21 21:41:43 +0530

    Update main.jsx

--- Projects-vite+react/vite-project/src/main.jsx
+++ Projects-vite+react/vite-project/src/main.jsx
@@ -8,3 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 )
+
+//https://www.figma.com/file/bUGIPys15E78w9bs1l4tgS/HooBank?node-id=0-1&t=jn5eQHOOodpAmeLt-0
+//https://www.youtube.com/watch?v=_oO4Qi5aVZs
