4571deaec564cdb6f8bfdd5494ba050401bf0b2b
Carrion <Carrion18j@gmail.com>
2023-03-22 14:06:43 +0530

    Update Q2348.js

--- Js_Problems/Q2348.js
+++ Js_Problems/Q2348.js
@@ -5,18 +5,20 @@ A subarray is a contiguous non-empty sequence of elements within an array. */
 nums = [1, 3, 0, 0, 2, 0, 0, 4];
 
 let sunarray = 0;
+let i = 0;
 
-for (let i = 0; i < nums.length; i++) {
+while (i < nums.length) {
   nums.map(() => {
     if (nums[i] === 0) {
       sunarray++;
       if (nums[i] === nums[++i]) {
         sunarray++;
-        if (nums[i] === nums[++i] && nums[i] === nums[i + 2]) {
+        if (nums[i] === nums[++i] && nums[++i] === nums[i + 2]) {
           sunarray++;
         }
       }
     }
   });
+  i++;
 }
 console.log(sunarray);
