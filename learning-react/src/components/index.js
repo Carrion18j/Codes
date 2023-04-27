f365b3e4ffb4926c4365a6163848c4262d1abb4a
Carrion <Carrion18j@gmail.com>
2023-04-27 22:20:44 +0530

    .

--- learning-react/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Expences from "./Expences/Expences";
-import NewExpence from "./NewExpence/NewExpence"
-import MainMeter from "./Expences/ExpenceMeter/MainMeter";
-
-export {
-    Expences,
-    NewExpence,
-    MainMeter
-}
--- learning-react/src/styles/index.js
+++
@@ -1,6 +0,0 @@
-const styles = {
-    formDiv:'font-semibold text-[20px] m-[10px] flex justify-between flex-wrap',
-    flexs:'flex justify-center items-center'
-}
-
-export default styles;
